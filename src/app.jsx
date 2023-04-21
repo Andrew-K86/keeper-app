596d9001a9d6e1ec211d34906b21ba6f2ecafc31
Andrew <Kusnir8655@gmil.com>
2023-04-21 13:03:24 +0200

    first

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react";
+
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+
+
+function App() {
+    return
+}
+
+export default App;
